5b1d0f574b442a82e662d59c8687e12532504c2e
alansaid <alan@alans.se>
2015-03-26 13:27:32 +0100

    Cleaned up pom, removed files generated by build, cleaned up test cases and printouts, removed use of deprecated methods, removed files generated by IDE

--- src/test/java/edu/jhu/nlp/wikipedia/TestCase.java
+++ src/test/java/edu/jhu/nlp/wikipedia/TestCase.java
@@ -1,18 +1,23 @@
 package edu.jhu.nlp.wikipedia;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * 
+ *
  * @author Delip Rao
  *
  */
 public class TestCase {
+  private static final Logger logger = LoggerFactory.getLogger(TestCase.class);
+
   public void check(String testCase, boolean result) {
     try {
       if(result)
-        System.err.println("Test '" + testCase + "' passed");
+        logger.info("Test '" + testCase + "' passed");
       else throw new Exception("Test '" + testCase + "' failed");
     } catch(Exception e) {
-      e.printStackTrace();
+      logger.error(e.toString());
       System.exit(-1);
     }
   }
--- src/test/java/edu/jhu/nlp/wikipedia/WikiXMLParserTest.java
+++ src/test/java/edu/jhu/nlp/wikipedia/WikiXMLParserTest.java
@@ -2,6 +2,8 @@ package edu.jhu.nlp.wikipedia;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class WikiXMLParserTest {
     @Test
     public void testSaxParser() throws Exception
@@ -11,11 +13,10 @@ public class WikiXMLParserTest {
         wxsp.setPageCallback(new PageCallbackHandler() {
             @Override
             public void process(WikiPage page) {
-                System.out.println(page.getTitle());
-                for (String link : page.getLinks())
-                {
-                    System.out.println(link);
-                }
+
+                assertEquals("Isaac Newton", page.getTitle());
+                assertEquals("14627", page.getID());
+
             }
         });
         wxsp.parse();
