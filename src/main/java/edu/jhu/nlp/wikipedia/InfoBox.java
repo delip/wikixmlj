62c01edc4e5e5dd135c2a1dc8f3e31b8343bb902
treedust <volivare@ualberta.ca>
2016-08-31 17:20:16 -0600

    Revert "Merge branch 'infobox'"
    This reverts commit ffd5290b3a8df2540f102cdf8a097cf9f05d309b, reversing
    changes made to 763cf4a64abc731bc394b7743276755cb19e2a8d.

--- src/main/java/edu/jhu/nlp/wikipedia/InfoBox.java
+++ src/main/java/edu/jhu/nlp/wikipedia/InfoBox.java
@@ -3,24 +3,13 @@ package edu.jhu.nlp.wikipedia;
 /**
  * A class abstracting Wiki infobox
  * @author Delip Rao
- * @author Victor Olivares
  */
 public class InfoBox {
-	String infoBoxWikiText = null;
-	InfoBox(String infoBoxWikiText) {
-		//to to be the following line
-		//this.infoBoxWikiText = infoBoxWikiText;
-		if (infoBoxWikiText != null){
-			this.infoBoxWikiText = infoBoxWikiText;
-		} else {
-			//set infobox text to empty string
-			this.infoBoxWikiText = new String();
-		}
-	}
-	public String dumpRaw() {
-		return infoBoxWikiText;
-	}
-	public boolean isEmpty() {
-		return infoBoxWikiText.isEmpty();
-	}
+  String infoBoxWikiText = null;
+  InfoBox(String infoBoxWikiText) {
+    this.infoBoxWikiText = infoBoxWikiText;
+  }
+  public String dumpRaw() {
+    return infoBoxWikiText;
+  }
 }
